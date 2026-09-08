418c6907f3ed65e0f8fe8269ea5ae2856dcd5656
ezh3ee <ezh3ee@gmail.com>
2026-09-08 03:23:33 +0300

    more small fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['dist', 'node_modules', 'eslint.config.mjs'],
+    ignores: ['dist', 'node_modules', 'eslint.config.mjs', 'src/generated'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
